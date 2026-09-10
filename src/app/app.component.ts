5b79b72b08264bafb99ea54d1965c49274868ebd
AnilKumar-Aratikatla <anilkumara@sourabinfotech.in>
2026-09-10 13:04:43 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import * as AOS from 'aos';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+ ngAfterViewInit(): void {
+  AOS.init({
+    duration: 1000,
+    once: false,
+    offset: 100
+  });
+
+  AOS.refresh();
+}
+
+
+  title = 'OZO_Admin';
+}
